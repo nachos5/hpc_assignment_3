5c037ae4444096c4366176a9983cd4f618928a1e
nachos5 <gnorland93@gmail.com>
2019-11-29 22:32:28 +0000

    x

--- fish_folder/utils.c
+++ fish_folder/utils.c
@@ -1,7 +1,7 @@
 #include "./utils.h"
 
 // in this case we only need one landcell (the harbor), the rest is water
-void generateType(int *rank, int *outbuf, int *harbor_rank, int *harbor_coords)
+void generateTileType(int *rank, int *outbuf, int *harbor_rank, int *harbor_coords)
 {
   // same seed to make sure we get the same result for all processes
   srand(time(NULL));
@@ -24,7 +24,7 @@ void generateType(int *rank, int *outbuf, int *harbor_rank, int *harbor_coords)
 
 // function to visualize the grid, the root process gathers the type of all cells (processes)
 // and then prints the content in the right order
-void visualizeGrid(int *rank, int *outbuf, int *fish_ranks, int *boat_ranks)
+void visualizeGrid(int *rank, int *outbuf, int *fish_ranks, int *boat_ranks, int *boat_has_fish_group)
 {
   MPI_Barrier(MPI_COMM_WORLD);
   // only the root process gathers the data and prints it
@@ -45,13 +45,17 @@ void visualizeGrid(int *rank, int *outbuf, int *fish_ranks, int *boat_ranks)
       // fish
       if (fish_ranks[0] == recv_rank_buff[i])
       {
-        strengur[3] = 'f';
-        strengur[4] = '1';
+        if (boat_has_fish_group[0] != 1 && boat_has_fish_group[1] != 1) {
+          strengur[3] = 'f';
+          strengur[4] = '1';
+        }
       }
       else if (fish_ranks[1] == recv_rank_buff[i])
       {
-        strengur[3] = 'f';
-        strengur[4] = '2';
+        if (boat_has_fish_group[1] != 2 && boat_has_fish_group[1] != 2) {
+          strengur[3] = 'f';
+          strengur[4] = '2';
+        }
       }
       // boats
       if (boat_ranks[0] == recv_rank_buff[i])
@@ -83,15 +87,15 @@ void visualizeGrid(int *rank, int *outbuf, int *fish_ranks, int *boat_ranks)
   MPI_Barrier(MPI_COMM_WORLD);
 }
 
-void obj_print_coordinates(int *type, int *index, int *coords, int *fish_group_size)
+void obj_print_coordinates(int type, int *index, int *coords, int *fish_group_size)
 {
-  if (*type == 1)
+  if (type == FISH)
   {
-    printf("Fish group %d of size %d is at coordinates %d,%d \n", *index, fish_group_size[*index - 1], coords[0], coords[1]);
+    printf("Fish group %d of size %d moves to coordinates %d,%d \n", *index + 1, fish_group_size[*index], coords[0], coords[1]);
   }
-  else if (*type == 2)
+  else if (type == BOAT)
   {
-    printf("Boat %d is at coordinates %d,%d \n", *index, coords[0], coords[1]);
+    printf("Boat %d moves to coordinates %d,%d \n", *index + 1, coords[0], coords[1]);
   }
 }
 
@@ -100,15 +104,15 @@ void obj_print_coordinates(int *type, int *index, int *coords, int *fish_group_s
  */
 int max(int num1, int num2)
 {
-    return (num1 > num2 ) ? num1 : num2;
+  return (num1 > num2) ? num1 : num2;
 }
 
 /**
  * Find minimum between two numbers.
  */
-int min(int num1, int num2) 
+int min(int num1, int num2)
 {
-    return (num1 > num2 ) ? num2 : num1;
+  return (num1 > num2) ? num2 : num1;
 }
 
 int coords_to_rank(int x, int y)
@@ -116,7 +120,7 @@ int coords_to_rank(int x, int y)
   return (x * COLS) + y;
 }
 
-int* rank_to_coords(int rank)
+int *rank_to_coords(int rank)
 {
   int x = rank / COLS;
   int y = rank % COLS;
@@ -128,26 +132,34 @@ int* rank_to_coords(int rank)
 
 int manhattan_distance(int *p1, int *p2)
 {
-  return abs(p1[0] - p1[1]) + abs(p2[0] - p2[1]);
+  return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
 }
 
 int towards_harbor(int *nbrs, int *harbor_rank)
 {
   int min_dist = 1000;
   int min_index = 1000;
-  int* harbor_coords = rank_to_coords(*harbor_rank);
+  int *harbor_coords = rank_to_coords(*harbor_rank);
   // because our grid is periodic we need to check "both ways"
-  int* harbor_coords_other_way = rank_to_coords(*harbor_rank + COLS);
+  int *harbor_coords_other_way = rank_to_coords(*harbor_rank);
+  harbor_coords_other_way[1] = harbor_coords_other_way[1] + COLS;
+  // printf("harbor %d, %d, %d, %d \n", harbor_coords[0], harbor_coords[1],
+  //        harbor_coords_other_way[0], harbor_coords_other_way[1]);
+  
   for (int i = 0; i < 4; i++)
   {
     int dist;
-    int* curr_coords = rank_to_coords(nbrs[i]);
+    int *curr_coords = rank_to_coords(nbrs[i]);
+    // printf("curr coords %d, %d \n", curr_coords[0], curr_coords[1]);
     dist = min(manhattan_distance(harbor_coords, curr_coords), manhattan_distance(harbor_coords_other_way, curr_coords));
     min_dist = min(min_dist, dist);
-    if (min_dist == dist) {
+    // printf("dist og min dist %d, %d \n", dist, min_dist);
+    if (min_dist == dist)
+    {
       min_index = i;
     }
   }
   // we return the index neighbour
+  // printf("min index: %d %d\n", min_index, nbrs[min_index]);
   return min_index;
 }
--- fish_folder/utils.h
+++ fish_folder/utils.h
@@ -13,6 +13,9 @@
 #define SIZE 16
 #define ROWS 4
 #define COLS 4
+#define FISH 1
+#define BOAT 2
+#define ITERATIONS 30
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,9 +23,9 @@
 #include <unistd.h>
 #include <mpi.h>
 
-void generateType(int *rank, int *outbuf, int *harbor_rank, int *harbor_coords);
-void visualizeGrid(int *rank, int *outbuf, int *fish_ranks, int *boat_ranks);
-void obj_print_coordinates(int *type, int *index, int *coords, int *fish_group_size);
+void generateTileType(int *rank, int *outbuf, int *harbor_rank, int *harbor_coords);
+void visualizeGrid(int *rank, int *outbuf, int *fish_ranks, int *boat_ranks, int *boat_has_fish_group);
+void obj_print_coordinates(int type, int *index, int *coords, int *fish_group_size);
 int max(int num1, int num2);
 int min(int num1, int num2);
 int coords_to_rank(int x, int y);
